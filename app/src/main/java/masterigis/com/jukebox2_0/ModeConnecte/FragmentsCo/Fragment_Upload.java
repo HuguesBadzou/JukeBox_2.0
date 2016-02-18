5b2626e222655edfc9c9f999fb9efeabf527b886
Mahafous Mama <mamabm1@yahoo.fr>
2016-02-18 11:28:45 +0100

    Ajout des classes fragments

---
+++ app/src/main/java/masterigis/com/jukebox2_0/ModeConnecte/FragmentsCo/Fragment_Upload.java
@@ -0,0 +1,35 @@
+package masterigis.com.jukebox2_0.ModeConnecte.FragmentsCo;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import m1geii.com.jukebox2_0.R;
+
+public class Fragment_Upload extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_upload, container, false);
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
